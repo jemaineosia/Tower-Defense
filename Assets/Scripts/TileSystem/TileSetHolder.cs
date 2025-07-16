14e09d1e7c9196e078a62817bd42c850fa8486c6
jemaineosia <jemaine.osia@gmail.com>
2025-07-17 02:28:45 +0800

    added kenny assets / created new level in scene1

--- Assets/Scripts/TileSystem/TileSetHolder.cs
+++ Assets/Scripts/TileSystem/TileSetHolder.cs
@@ -2,24 +2,52 @@ using UnityEngine;
 
 public class TileSetHolder : MonoBehaviour
 {
-    public GameObject tileRoad;
-    public GameObject tileField;
-    public GameObject tileSideway;
+    //public GameObject tileRoad;
+    //public GameObject tileField;
+    //public GameObject tileSideway;
 
-    [Header("Corners")]
-    public GameObject tileInnerCorner;
-    public GameObject tileInnerCornerSmall;
-    public GameObject tileOuterCorner;
-    public GameObject tileOuterCornerSmall;
+    //[Header("Corners")]
+    //public GameObject tileInnerCorner;
+    //public GameObject tileInnerCornerSmall;
+    //public GameObject tileOuterCorner;
+    //public GameObject tileOuterCornerSmall;
 
-    [Header("Hills")]
-    public GameObject tileHill1;
-    public GameObject tileHill2;
-    public GameObject tileHill3;
+    //[Header("Hills")]
+    //public GameObject tileHill1;
+    //public GameObject tileHill2;
+    //public GameObject tileHill3;
 
-    [Header("Bridges")]
-    public GameObject tileBridgeField;
-    public GameObject tileBridgeRoad;
-    public GameObject tileBridgeSideway;
+    //[Header("Bridges")]
+    //public GameObject tileBridgeField;
+    //public GameObject tileBridgeRoad;
+    //public GameObject tileBridgeSideway;
+
+    [Header("Kenny Assets")]
+    public GameObject snowTile;
+    public GameObject snowTileDirt;
+    public GameObject snowTileStraight;
+    public GameObject snowTileCorner;
+    public GameObject snowTileBump;
+    public GameObject snowTileCornerInner;
+    public GameObject snowTileCornerOuter;
+    public GameObject snowTileCornerRound;
+    public GameObject snowTileCornerSquare;
+    public GameObject snowTileCrossing;
+    public GameObject snowTileEnd;
+    public GameObject snowTileEndRound;
+    public GameObject snowTileHill;
+    public GameObject snowTileRock;
+    public GameObject snowTileSlope;
+    public GameObject snowTileSplit;
+    public GameObject snowTileStraightSlope;
+    public GameObject snowTileStraightSlopeLarge;
+    public GameObject snowTileTransition;
+
+    [Header("Kenny Decorations")]
+    public GameObject snowTileCrystal;
+    public GameObject snowTileTreeQuad;
+    public GameObject snowTileTreeDouble;
+    public GameObject snowTileTree;
+
 
 }
--- Assets/Scripts/TileSystem/TileSlotEditor.cs
+++ Assets/Scripts/TileSystem/TileSlotEditor.cs
@@ -56,105 +56,166 @@ public class TileSlotEditor : Editor
         }
         GUILayout.EndHorizontal();
 
-        GUILayout.Label("Tile Options", centeredStyle);
+        //GUILayout.Label("Tile Options", centeredStyle);
+        //GUILayout.BeginHorizontal();
+        //if(GUILayout.Button("Field", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileField;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Road", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileRoad;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Sideway", GUILayout.Width(oneButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileSideway;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+
+        //GUILayout.Label("Corner Options", centeredStyle);
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Inner Corner", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileInnerCorner;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Outer Corner", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileOuterCorner;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Small Inner Corner", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileInnerCornerSmall;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Small Outer Corner", GUILayout.Width(twoButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileOuterCornerSmall;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+
+        //GUILayout.Label("Bridges and Hills", centeredStyle);
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Hill 1", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill1;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Hill 2", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill2;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Hill 3", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill3;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+        
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Bridge with Field", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeField;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Bridge with Road", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeRoad;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //if (GUILayout.Button("Bridge with Sideway", GUILayout.Width(threeButtonWidth)))
+        //{
+        //    GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeSideway;
+
+        //    foreach (var targetTile in targets)
+        //    {
+        //        ((TileSlot)targetTile).SwitchTile(newTile);
+        //    }
+        //}
+        //GUILayout.EndHorizontal();
+
+        GUILayout.Label("Kenny Assets", centeredStyle);
         GUILayout.BeginHorizontal();
-        if(GUILayout.Button("Field", GUILayout.Width(twoButtonWidth)))
+        if (GUILayout.Button("Tile", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileField;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTile;
 
             foreach (var targetTile in targets)
             {
                 ((TileSlot)targetTile).SwitchTile(newTile);
             }
         }
-        if (GUILayout.Button("Road", GUILayout.Width(twoButtonWidth)))
+        if (GUILayout.Button("Dirt", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileRoad;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileDirt;
 
             foreach (var targetTile in targets)
             {
                 ((TileSlot)targetTile).SwitchTile(newTile);
             }
         }
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Sideway", GUILayout.Width(oneButtonWidth)))
+        if (GUILayout.Button("Bump", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileSideway;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        GUILayout.EndHorizontal();
-
-        GUILayout.Label("Corner Options", centeredStyle);
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Inner Corner", GUILayout.Width(twoButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileInnerCorner;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        if (GUILayout.Button("Outer Corner", GUILayout.Width(twoButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileOuterCorner;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Small Inner Corner", GUILayout.Width(twoButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileInnerCornerSmall;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        if (GUILayout.Button("Small Outer Corner", GUILayout.Width(twoButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileOuterCornerSmall;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        GUILayout.EndHorizontal();
-
-        GUILayout.Label("Bridges and Hills", centeredStyle);
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Hill 1", GUILayout.Width(threeButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill1;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        if (GUILayout.Button("Hill 2", GUILayout.Width(threeButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill2;
-
-            foreach (var targetTile in targets)
-            {
-                ((TileSlot)targetTile).SwitchTile(newTile);
-            }
-        }
-        if (GUILayout.Button("Hill 3", GUILayout.Width(threeButtonWidth)))
-        {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileHill3;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileBump;
 
             foreach (var targetTile in targets)
             {
@@ -164,27 +225,87 @@ public class TileSlotEditor : Editor
         GUILayout.EndHorizontal();
         
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Bridge with Field", GUILayout.Width(threeButtonWidth)))
+        if (GUILayout.Button("Corner", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeField;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCorner;
 
             foreach (var targetTile in targets)
             {
                 ((TileSlot)targetTile).SwitchTile(newTile);
             }
         }
-        if (GUILayout.Button("Bridge with Road", GUILayout.Width(threeButtonWidth)))
+        if (GUILayout.Button("Straight", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeRoad;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileStraight;
 
             foreach (var targetTile in targets)
             {
                 ((TileSlot)targetTile).SwitchTile(newTile);
             }
         }
-        if (GUILayout.Button("Bridge with Sideway", GUILayout.Width(threeButtonWidth)))
+        if (GUILayout.Button("Crossing", GUILayout.Width(threeButtonWidth)))
         {
-            GameObject newTile = FindFirstObjectByType<TileSetHolder>().tileBridgeSideway;
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCrossing;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Corner Inner", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCornerInner;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Corner Outer", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCornerOuter;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Corner Round", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCornerRound;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Hill", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileHill;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Slope", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileSlope;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Split", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileSplit;
 
             foreach (var targetTile in targets)
             {
@@ -194,6 +315,126 @@ public class TileSlotEditor : Editor
         GUILayout.EndHorizontal();
 
 
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Corner Square", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCornerSquare;
 
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("End", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileEnd;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("End Round", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileEndRound;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Straight Slope", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileStraightSlope;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Straight Slope Large", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileStraightSlopeLarge;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Transition", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileTransition;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Crystal", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileCrystal;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Trees Quad", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileTreeQuad;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Tree Single", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileTree;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Rock", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileRock;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Hill", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileHill;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        if (GUILayout.Button("Tree Double", GUILayout.Width(threeButtonWidth)))
+        {
+            GameObject newTile = FindFirstObjectByType<TileSetHolder>().snowTileTreeDouble;
+
+            foreach (var targetTile in targets)
+            {
+                ((TileSlot)targetTile).SwitchTile(newTile);
+            }
+        }
+        GUILayout.EndHorizontal();
     }
 }
