90f0b6a67b4234e49e45bb5bfab8ec2bdb65db69
jemaineosia <jemaine.osia@gmail.com>
2025-07-20 10:15:06 +0800

    animated the build buttons

--- Assets/Scripts/UI/UI_BuildButtons.cs
+++ Assets/Scripts/UI/UI_BuildButtons.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class UI_BuildButtons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float yPositionOffset;
+    
+    public bool isActive;
+    private UI_Animator uiAnimator;
+
+    private void Awake()
     {
-        
+        uiAnimator = GetComponentInParent<UI_Animator>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("B key pressed - Toggling build buttons");
+            ToggleBuildButtons();
+        }
+    }
+
+    public void ToggleBuildButtons()
+    { 
+        isActive = !isActive;
+
+        float yOffset = isActive ? -yPositionOffset : yPositionOffset;
+        Vector3 offset = new Vector3(0, yOffset);
+
+        uiAnimator.ChangePosition(transform, offset);
     }
 }
